14b136f815aefde7d2c2e7a15392cc6fbd61670e
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-11-15 17:31:17 +0100

    implement express.json() middleware

--- src/api/handlerValidate.ts
+++ src/api/handlerValidate.ts
@@ -5,26 +5,12 @@ export function handlerChirpsValidate(req: Request, res: Response) {
   type parameters = {
     body: string
   }
+  const params: parameters = req.body
 
-  let body = ''
-  req.on('data', (chunk) => {
-    body += chunk
-  })
+  if (params.body.length > 140) {
+    respondWithError(res, 400, 'Chirp is too long')
+    return
+  }
 
-  let params: parameters
-  req.on('end', () => {
-    try {
-      params = JSON.parse(body)
-    } catch (e) {
-      respondWithError(res, 400, 'Invalid JSON')
-      return
-    }
-
-    if (params.body.length > 140) {
-      respondWithError(res, 400, 'Chirp is too long')
-      return
-    }
-
-    respondWithJSON(res, 200, { valid: true })
-  })
+  respondWithJSON(res, 200, { valid: true })
 }
