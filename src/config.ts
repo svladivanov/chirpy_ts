5d1869dcf0d7dec45d0dcf28e57d65d1d4752170
Vlad Ivanov <vlad.ivanov@hotmail.com>
2025-11-16 12:13:11 +0100

    implement first db query

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,18 @@
 type APIConfig = {
   fileserverHits: number
+  dbUrl: string
 }
 
+process.loadEnvFile()
+
+function envOrThrow(key: string) {
+  const value = process.env[key]
+  if (!value) {
+    throw new Error(`Environment variable ${key} is not set`)
+  }
+  return value
+}
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbUrl: envOrThrow('DB_URL'),
 }
---
+++ src/db/queries/users.ts
@@ -0,0 +1,12 @@
+import { db } from '../index.js'
+import { NewUser, User, users } from '../schema.js'
+
+export async function createUser(user: NewUser): Promise<User> {
+  const [result] = await db
+    .insert(users)
+    .values(user)
+    .onConflictDoNothing()
+    .returning()
+
+  return result
+}
